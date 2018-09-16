64883976c6cafdde32af421db773cf072eb684c5
Lucas Mateus <31706781+lukinhasm07@users.noreply.github.com>
2018-09-16 12:38:13 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,24 @@
+public class No {
+	private Object elemento;
+    private No proximo;
+    private No anterior;
+    
+	public Object getElemento() {
+		return elemento;
+	}
+	public void setElemento(Object elemento) {
+		this.elemento = elemento;
+	}
+	public No getProximo() {
+		return proximo;
+	}
+	public void setProximo(No proximo) {
+		this.proximo = proximo;
+	}
+	public No getAnterior() {
+		return anterior;
+	}
+	public void setAnterior(No anterior) {
+		this.anterior = anterior;
+	}
+}
