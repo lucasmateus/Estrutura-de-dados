7388cbc697bfe98fd13f6c40a5a0b53097ad2ccd
Lucas Mateus <31706781+lukinhasm07@users.noreply.github.com>
2019-03-29 13:54:32 -0300

    Tudo certo!

--- ArvoreAVL/src/Main.java
+++ ArvoreAVL/src/Main.java
@@ -1,17 +1,15 @@
-
 public class Main {
-
-	public static void main(String[] args) {
-		ArvoreAVL arv = new ArvoreAVL(new NoAVL(null,"Lucas", 6));
-		arv.inserir("Mateus", 5);
-		arv.inserir("Silva", 7);
-		arv.inserir("Coleguinhha", 4);
-		arv.remover(6);
-		System.out.println(arv.root().getElemento());
-		System.out.println(arv.root().getFb());
-		System.out.println(arv.root().getEsq().getElemento());
-		System.out.println(arv.root().getEsq().getFb());
-		System.out.println(arv.root().getDir().getElemento());
-		//System.out.println(arv.Busca(14).getElemento());
-	}
+    public static void main(String[] args) {
+        ArvoreAVL arv = new ArvoreAVL(new NoAVL(null,"Lucas", 6));
+	arv.inserir("Mateus", 5);
+	arv.inserir("Silva", 7);
+	arv.inserir("Coleguinhha", 3);
+        arv.inserir("Isaque", 4);
+	arv.remover(6);
+        arv.remover(3);
+        arv.inserir("Daniel", 8);
+        arv.inserir("Pedro", 9);
+        arv.inserir("Israel", 3);
+        arv.display();
+    }
 }
