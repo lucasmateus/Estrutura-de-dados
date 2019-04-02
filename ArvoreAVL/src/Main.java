6dc4c476398287fe643aa7124ef4f120c8c7aa60
Lucas Mateus <31706781+lukinhasm07@users.noreply.github.com>
2019-04-02 02:47:36 -0300

    Update Main.java

--- ArvoreAVL/src/Main.java
+++ ArvoreAVL/src/Main.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        ArvoreAVL arv = new ArvoreAVL(new NoAVL(null,"Lucas", 6));
-	arv.inserir("Mateus", 5);
-	arv.inserir("Silva", 7);
-	arv.inserir("Coleguinhha", 3);
-        arv.inserir("Isaque", 4);
-	arv.remover(6);
-        arv.remover(3);
-        arv.inserir("Daniel", 8);
-        arv.inserir("Pedro", 9);
-        arv.inserir("Israel", 3);
-        arv.display();
-    }
+	public static void main(String[] args) {
+		ArvoreAVL arv = new ArvoreAVL();
+        /*int op = -1;
+        Scanner sc = new Scanner(System.in);
+        while(op != 0){
+            System.out.println("1- inserir 2- remover");
+            op = sc.nextInt();
+            if(op == 1){
+                System.out.println("digite o objeto a ser inserida:");
+                String str = sc.next();
+                System.out.println("digite o chave a ser inserida:");
+                int x = sc.nextInt();
+                arv.inserir(str,x);
+            }
+            if(op == 2){
+                System.out.println("digite chave a ser removida:");
+                if(arv.remover(sc.nextInt())==null)
+                    System.out.println("chave inexistente");
+            }
+            arv.display();
+        }*/
+		arv.inserir("lucas",10);
+		arv.inserir("Israel",20);
+		arv.inserir("Pedro",30);
+		arv.inserir("Lucas Santos",40);
+		arv.inserir("Daniel Souza",50);
+		arv.inserir("Coleguinha",60);
+		arv.inserir("Andre",70);
+		arv.inserir("Isaque",80);
+		arv.display();
+		arv.remover(30);
+		arv.display();
+		arv.remover(10);
+		arv.display();
+	}
 }
