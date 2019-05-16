e3933af22bf332ede412c4de17720c80ddd19d13
Lucas Mateus <31706781+lukinhasm07@users.noreply.github.com>
2019-05-16 15:07:04 -0300

    Update Main.java

--- Grafos/src/Main.java
+++ Grafos/src/Main.java
@@ -24,9 +24,9 @@ public class Main {
 				case "2":
 					System.out.print("Digite o nome da Aresta: ");
 					String elementoAresta = sc.nextLine();
-					System.out.print("\nDigite a posiÁ„o do Vertice 1: ");
+					System.out.print("\nDigite a posi√ß√£o do Vertice 1: ");
 					int p1 = sc.nextInt()-1;
-					System.out.print("\nDigite a posiÁ„o do Vertice 2: ");
+					System.out.print("\nDigite a posi√ß√£o do Vertice 2: ");
 					int p2 = sc.nextInt()-1;
 					g.inserirAresta(elementoAresta, p1, p2, false);
 					System.out.println();
@@ -34,18 +34,30 @@ public class Main {
 					break;
 				
 				case "3":
+					System.out.print("Digite o nome da Aresta: ");
+					String elementoArestaDirecionada = sc.nextLine();
+					System.out.print("\nDigite a posi√ß√£o do Vertice de saida: ");
+					int pd1 = sc.nextInt()-1;
+					System.out.print("\nDigite a posi√ß√£o do Vertice de entrada: ");
+					int pd2 = sc.nextInt()-1;
+					g.inserirArestaDirecionada(elementoArestaDirecionada, pd1, pd2, true);
+					System.out.println();
+					Menu();
+					break;
+						
+				case "4":
 					System.out.println("--------------Tabela----------------");
 					g.displayTabela();
 					Menu();
 					break;
-					
+				
 				case "0":
-					System.out.println("Finalizando aplicaÁ„o");
+					System.out.println("Finalizando aplica√ß√£o");
 					marcador = false;
 					break;
 				
 				default:
-					System.out.println("Opc„o invalida! Por favor escolha uma opc„o valida.");
+					System.out.println("Opc√£o invalida! Por favor escolha uma opc√£o valida.");
 					Menu();
 					break;
 			}
@@ -59,7 +71,8 @@ public class Main {
 				"\n------------Menu------------\n"+
 				"1 - Adicionar Vertice\n"+
 				"2 - Adicionar Aresta Simples\n"+
-				"3 - Mostrar Tabela\n"+
+				"3 - Adicionar Aresta Direcionada\n"+
+				"4 - Mostrar Tabela\n"+
 				"0 - Sair\n"
 		);
 	}
