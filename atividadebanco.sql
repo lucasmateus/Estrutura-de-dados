ab19d62255f223df7b1b4de73fb24a9095965512
Lucas Mateus <31706781+lucasmateusweb@users.noreply.github.com>
2019-09-30 23:06:53 -0300

    Update atividadebanco.sql

--- atividadebanco.sql
+++ atividadebanco.sql
@@ -58,3 +58,63 @@ end
 drop function SalesMonthIncrease
 select * from SalesMonthIncrease(01, 2014) order by Identificador
 select * from SalesMonth(12,2013) order by Identificador
+
+
+
+/* 3.	Crie uma função de nome CategorySalesMonth que recebe como parâmetro um numero de mês e um numero de ano e apresenta uma lista com os totais vendidos por categoria de produto no mês e 
+	ano passados como parâmetro. A lista de apresentar as seguintes informações sobre as categorias: 
+	Identificador da Categoria (ProductCategoryID), Nome da Categoria (ProductCategory), Mês, Ano, Quantidade de Itens Vendidos, Preço Unitário Médio do Item, Valor Total Vendido. 
+*/
+DROP FUNCTION CategorySalesMonth;
+CREATE FUNCTION CategorySalesMonth(
+	@month int,
+	@year int
+)
+RETURNS TABLE
+AS
+RETURN
+	SELECT psc.ProductCategoryID as 'Identificador da Categoria', pc.Name as 'Nome da Categoria', @month as 'Mês', @year as 'Ano', 
+	SUM(sod.OrderQty) as 'Quantidade De Itens Vendidos', 
+	AVG(sod.UnitPrice) as 'Preço Unitário Médio do Item', 
+	SUM(sod.LineTotal) as 'Valor Total Vendido'
+	FROM Production.Product p
+	INNER JOIN Production.ProductSubcategory psc
+		ON psc.ProductSubcategoryID = p.ProductSubcategoryID
+	INNER JOIN Production.ProductCategory pc
+		ON pc.ProductCategoryID = psc.ProductCategoryID
+	INNER JOIN Sales.SalesOrderDetail sod
+		ON sod.ProductID = p.ProductID
+	INNER JOIN Sales.SalesOrderHeader soh
+		ON soh.SalesOrderID = sod.SalesOrderID
+	WHERE MONTH(soh.OrderDate) = @month
+	AND YEAR(soh.OrderDate) = @year
+	GROUP BY psc.ProductCategoryID, pc.Name;
+
+SELECT * FROM CategorySalesMonth(3, 2014);
+
+/*  4.	Crie uma função de nome CategoryTopIncrease que recebe como parâmetro um numero de mês e um numero de ano e apresenta dados de vendas da categoria de produtos que teve maior aumento 
+	de venda na comparação do mesm informado com o mesmo mês do ano anterior. A função deve apresentar as seguintes informações sobre a categoria: 
+	Identificador da Categoria (ProductCategoryID), Nome da Categoria (ProductCategory), Mês, Ano, Quantidade Total Vendida (ano anterior), Valor Total Vendido (ano anterior), 
+	Quantidade Total Vendida (mês informado), Valor Total Vendido (mês informado), percentual de aumento de vendas (em relação ao valor total vendido).
+*/
+
+SELECT TOP 1 [Identificador da Categoria] FROM CategorySalesMonth(2, 2014) ORDER BY [Valor Total Vendido] DESC;
+
+CREATE FUNCTION CategoryTopIncrease (
+	@month int,
+	@year int
+)
+RETURNS TABLE
+AS
+RETURN
+SELECT TOP 1 csmatual.[Identificador da Categoria], csmatual.[Nome da Categoria], @month as 'Mês', @year as 'Ano', 
+	csmanterior.[Quantidade De Itens Vendidos] as 'Quantidade Total Vendida (ano anterior)', csmanterior.[Valor Total Vendido] as 'Valor Total Vendido (ano anterior)',
+	csmatual.[Quantidade De Itens Vendidos] as 'Quantidade Total Vendida (mês informado)', csmatual.[Valor Total Vendido] as 'Valor Total Vendido (mês informado)',
+	((csmatual.[Valor Total Vendido] - csmanterior.[Valor Total Vendido]) / csmanterior.[Valor Total Vendido]) * 100 as 'Percentual de aumento de vendas (em relação ao valor total vendido)'
+FROM
+CategorySalesMonth(@month, @year) csmatual
+INNER JOIN 
+CategorySalesMonth(@month, @year - 1) csmanterior
+ON csmatual.[Identificador da Categoria] = csmanterior.[Identificador da Categoria]
+WHERE csmatual.[Valor Total Vendido] > csmanterior.[Valor Total Vendido]
+ORDER BY ((csmatual.[Valor Total Vendido] - csmanterior.[Valor Total Vendido]) / csmanterior.[Valor Total Vendido]) ASC;
